1e53943ce941918c344553d1695a88bd6b95e315
yas-nyan <yas-nyan@sfc.wide.ad.jp>
2018-03-09 15:20:14 +0900

    自動保存まで実装

--- src/components/Workingspace.vue
+++ src/components/Workingspace.vue
@@ -3,15 +3,37 @@
     <b-card no-body>
       <b-tabs card>
         <!-- Render Tabs -->
-        <b-tab :title="`Tab ${i}`" v-for="i in tabs" :key="i">
-          Tab Contents {{i}}
-          aaaaaaaaaaaaaaa
+        <b-tab :title="`${tab.title}`" v-for="tab in tabs" :key="tab.id">
+          <b-row>
+            <b-col lg="1">
+              <label :for="`title-${tab.id}`">タイトル:</label>
+            </b-col>
+            <b-col lg="4">
+              <b-input :id="`title-${tab.id}`" class="" v-model="tab.title"></b-input>
+            </b-col>
+            <b-col lg="1">
+              <label :for="`target-${tab.id}`" >目標数:</label>
+            </b-col>
+            <b-col lg="1">
+              <b-input :id="`target-${tab.id}`" class="" v-model="tab.target" type="number"></b-input>
+            </b-col>
+            <b-col lg="2">
+              現在の文字数：{{tab.content.length}}
+            </b-col>
+            <b-col lg="1">
+              <b-btn size="sm" variant="danger" class="float-right" @click="()=>closeTab(tab.id)">
+                ノートを削除
+              </b-btn>
+            </b-col>
+          </b-row>
+          <br>
+          <b-row>
+            <b-col lg="12">
+              <b-form-textarea v-model="tab.content" placeholder="何か書きましょう" :rows="30" :max-rows="40">
+              </b-form-textarea>
+            </b-col>
+          </b-row>
 
-          <b-input class="">
-          </b-input>
-          <b-btn size="sm" variant="danger" class="float-right" @click="()=>closeTab(i)">
-            Close tab
-          </b-btn>
         </b-tab>
 
         <!-- New Tab Button (Using tabs slot) -->
@@ -21,14 +43,14 @@
 
         <b-nav-item slot="tabs" class="danger" href="#">
           <b-btn size="sm" variant="danger" @click="()=>closeAllTabs()">
-            全てのタブを削除する
+            全てのノートを削除する
           </b-btn>
         </b-nav-item>
 
         <!-- Render this if no tabs -->
         <div slot="empty" class="text-center text-muted">
-          There are no open tabs
-          <br> Open a new tab using + button.
+          ノートがありません
+          <br> + ボタンから新しいノートを作成してください．
         </div>
       </b-tabs>
     </b-card>
@@ -45,22 +67,58 @@ export default {
       tabCounter: 0
     };
   },
+  created: function() {
+    this.apply();
+  },
+  watch: {
+    tabs: {
+      handler: function() {
+        //localstorageにtabsを保存
+        localStorage.setItem("data", JSON.stringify(this.tabs));
+        //counterも保存
+        localStorage.setItem("counter", this.tabCounter);
+        return this.tabs;
+      },
+      deep: true
+    },
+    tabCounter: function() {
+      localStorage.setItem("counter", this.tabCounter);
+      return this.tabCounter;
+    }
+  },
   methods: {
-    closeTab(x) {
-      for (let i = 0; i < this.tabs.length; i++) {
-        if (this.tabs[i] === x) {
-          this.tabs.splice(i, 1);
+    closeTab(id) {
+      for (let index in this.tabs) {
+        if (this.tabs[index].id === id) {
+          this.tabs.splice(index, 1);
         }
       }
     },
     newTab() {
-      this.tabs.push(this.tabCounter++);
+      //this.tabs.push(this.tabCounter++);
+      this.tabs.push({
+        id: this.tabCounter++,
+        title: "新しいノート",
+        content: "",
+        updated_at: new Date().toLocaleString(),
+        target: 0
+      });
     },
     closeAllTabs() {
       let is_ok = confirm("本当によろしいですか？");
       alert(this.tabs);
       if (is_ok) {
         this.tabs = [];
+      }
+    },
+    apply() {
+      try {
+        let old_tabs = JSON.parse(localStorage.getItem("data"));
+        let old_counter = Number(localStorage.getItem("counter"));
+        this.tabs = old_tabs;
+        this.tabCounter = old_counter;
+      } catch (e) {
+        console.log("無いよ");
       }
     }
   }
@@ -69,5 +127,8 @@ export default {
 <style lang="scss" scoped>
 .workingSpace {
   height: 500px;
+  label {
+    vertical-align: middle;
+  }
 }
 </style>
